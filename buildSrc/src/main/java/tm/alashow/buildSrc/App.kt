3abacabf34d559fd67bfa6837717b1efb443bee5
Alashov Berkeli <yunus.alashow@gmail.com>
2021-07-14 18:23:30 -0500

    Sdk version to 31 from S

--- buildSrc/src/main/java/tm/alashow/buildSrc/App.kt
+++ buildSrc/src/main/java/tm/alashow/buildSrc/App.kt
@@ -3,8 +3,8 @@ package tm.alashow.buildSrc
 object App {
     const val id = "tm.alashow.datmusic"
 
-    const val compileSdkVersion = "android-S"
-    const val targetSdkVersion = "S"
+    const val compileSdkVersion = 31
+    const val targetSdkVersion = 31
     const val minSdkVersion = 21
     const val versionCode = 1
     const val versionName = "2.0.0"
