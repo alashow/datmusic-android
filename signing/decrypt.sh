c1240e56ac499b7397faeeb8e09863780b3b6ed9
Alashov Berkeli <yunus.alashow@gmail.com>
2021-07-23 15:27:34 -0500

    Fix signing scripts release key file name

--- signing/decrypt.sh
+++ signing/decrypt.sh
@@ -3,7 +3,7 @@
 KEY=$1
 
 echo "Decrypting Release key"
-openssl aes-256-cbc -d -base64 -pbkdf2 -in signing/alashov-release.jks.aes -out signing/alashov-release.jks -k $KEY
+openssl aes-256-cbc -d -base64 -pbkdf2 -in signing/datmusic-release.jks.aes -out signing/datmusic-release.jks -k $KEY
 
 echo "Decrypting Play Store Account key"
 openssl aes-256-cbc -d -base64 -pbkdf2 -in signing/play-account.json.aes -out signing/play-account.json -k $KEY
