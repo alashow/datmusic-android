8bba762e1f03a7f60f3d2e6d18693efdb77dbc9f
Alashov Berkeli <yunus.alashow@gmail.com>
2021-08-13 05:05:29 -0500

    Use singleton coil image loader in player

--- modules/base-android/src/main/java/tm/alashow/base/imageloading/ImageLoading.kt
+++ modules/base-android/src/main/java/tm/alashow/base/imageloading/ImageLoading.kt
@@ -4,7 +4,14 @@
  */
 package tm.alashow.base.imageloading
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import coil.imageLoader
 import coil.request.ImageRequest
+import coil.request.SuccessResult
+import coil.size.Precision
 
 object ImageLoading {
 
@@ -14,5 +21,19 @@ object ImageLoading {
 
     fun ImageRequest.Builder.applyDefault() {
         crossfade(200)
+    }
+}
+
+suspend fun Context.getBitmap(uri: Uri, size: Int): Bitmap? {
+    val request = ImageRequest.Builder(this)
+        .data(uri)
+        .size(size)
+        .precision(Precision.INEXACT)
+        .allowHardware(true)
+        .build()
+
+    return when (val result = imageLoader.execute(request)) {
+        is SuccessResult -> (result.drawable as BitmapDrawable).bitmap
+        else -> null
     }
 }
