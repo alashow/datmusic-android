b3dcd515c6b99eca28ca531139dda4d9059b668f
Alashov Berkeli <yunus.alashow@gmail.com>
2021-11-27 06:45:41 -0600

    Prepare v2.1.72 release

--- buildSrc/src/main/java/tm/alashow/buildSrc/App.kt
+++ buildSrc/src/main/java/tm/alashow/buildSrc/App.kt
@@ -6,6 +6,6 @@ object App {
     const val compileSdkVersion = 31
     const val targetSdkVersion = 31
     const val minSdkVersion = 21
-    const val versionCode = 231
-    const val versionName = "2.1.71"
+    const val versionCode = 232
+    const val versionName = "2.1.72"
 }
