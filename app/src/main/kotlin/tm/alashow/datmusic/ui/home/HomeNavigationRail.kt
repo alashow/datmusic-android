7f162ae0d915587a01957d0d0c08f8f92d16e1c0
Alashov Berkeli <yunus.alashow@gmail.com>
2022-09-12 01:07:58 -0700

    Only apply systemBars insets to home navigation rail

--- app/src/main/kotlin/tm/alashow/datmusic/ui/home/HomeNavigationRail.kt
+++ app/src/main/kotlin/tm/alashow/datmusic/ui/home/HomeNavigationRail.kt
@@ -19,7 +19,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectableGroup
@@ -59,7 +59,7 @@ internal object HomeNavigationRailDefaults {
 
     val windowInsets: WindowInsets
         @Composable
-        get() = WindowInsets.safeDrawing
+        get() = WindowInsets.systemBars
             .only(WindowInsetsSides.Vertical + WindowInsetsSides.Start)
 
     val colors
