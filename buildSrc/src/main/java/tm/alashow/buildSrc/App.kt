cb173732eb439e45ea78b8336fcda860c80e6529
Berkeli Alashov <yunus.alashow@gmail.com>
2026-08-02 03:06:28 +0100

    Prepare for 2.5.0-beta.1 release

--- buildSrc/src/main/java/tm/alashow/buildSrc/App.kt
+++ buildSrc/src/main/java/tm/alashow/buildSrc/App.kt
@@ -7,5 +7,5 @@ object App {
     const val targetSdkVersion = 37
     const val minSdkVersion = 24
     const val versionCode = 248
-    const val versionName = "2.5.0-alpha"
+    const val versionName = "2.5.0-beta.1"
 }
